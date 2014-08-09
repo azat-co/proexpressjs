e95f98fe765a86980d3aa6beefee646e4dbaad96
Azat Mardan <hi@azat.co>
2014-08-09 02:45:04 -0700

    new chapter :hushed:

---
+++ ch2/hello-name.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+app.get('/name/:user_name', function(req,res) {
+  res.status(200);
+  res.set('Content-type', 'text/html');
+  res.end('<html><body>' +
+    '<h1>Hello ' + req.params.user_name + '</h1>' +
+    '</body></html>'
+  );
+});
+
+app.get('*', function(req, res){
+  res.end('Hello World');
+});
+
+app.listen(port, function(){
+  console.log('The server is running, ' +
+    ' please open your browser at http://localhost:%s',
+     port);
+});
