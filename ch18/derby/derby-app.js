de12d1bfe143b36755b6d57cce6ced6eb46967e7
Azat Mardan <hi@azat.co>
2014-09-13 06:53:21 -0700

    add new chapter:derby

--- ch18/derby/derby-app.js
+++ ch18/derby/derby-app.js
@@ -10,11 +10,11 @@ app.loadStyles(path.join(__dirname, 'styles', 'derby-app'));
 app.get('/', function(page, model, params) {
   model.subscribe('d.message', function() {
     page.render();
-  })
+  });
 });
 
 app.proto.create = function(model) {
   model.on('set', 'd.message', function(path, object) {
     console.log('message has been changed: ' + object);
-  })
+  });
 };
